2f469d37a004d34ac69677413b10cc8e73d6f290
Geilson25 <geilsonfidelis@egmail.com>
2021-06-12 17:35:18 -0300

    Container Profissionais - configurado.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
 import SafeEnvironment from 'ui/components/feedback/SafeEnvironment/SafeEnvironment';
 import PageTitle from 'ui/components/data-display/PageTitle/PageTitle';
 import UserInformation from 'ui/components/data-display/UserInformation/UserInformation';
+import TextFieldMask from 'ui/components/inputs/TextFieldMask/TextFieldMask';
+import { Button, Typography, Container } from '@material-ui/core';
+import { FormElementsContainer, ProfissionaisPaper, ProfissionaisContainer } from 'ui/styles/pages/index.style';
 
 export default function Home() {
   return(
@@ -11,13 +14,73 @@ export default function Home() {
         subtitle={'Preencha seu endereço e veja todos os profissionais da sua localidade.'}
         />
 
-      <UserInformation
-        name={'Geilson'}
-        picture={'https://github.com/geilson25.png'}
-        rating={3}
-        description={'Recife'}
+      <Container>
 
-      />
+        <FormElementsContainer>
+          <TextFieldMask 
+            mask={'99.999-999'}
+            label={'Digite seu CEP'} 
+            fullWidth
+            variant={'outlined'}
+          />
+
+          <Typography color={'error'} >CEP inválido</Typography>
+
+          <Button
+            variant={'contained'}
+            color={'secondary'}
+            sx={{ width: '220px' }}
+          >
+            Buscar
+          </Button>
+        </FormElementsContainer>
+
+
+        <ProfissionaisPaper>
+          <ProfissionaisContainer>
+            <UserInformation
+              name={'Geilson'}
+              picture={'https://github.com/geilson25.png'}
+              rating={3}
+              description={'Recife'}
+            />
+            <UserInformation
+              name={'Geilson'}
+              picture={'https://github.com/geilson25.png'}
+              rating={3}
+              description={'Recife'}
+            />
+            <UserInformation
+              name={'Geilson'}
+              picture={'https://github.com/geilson25.png'}
+              rating={3}
+              description={'Recife'}
+            />
+            <UserInformation
+              name={'Geilson'}
+              picture={'https://github.com/geilson25.png'}
+              rating={3}
+              description={'Recife'}
+            />
+            <UserInformation
+              name={'Geilson'}
+              picture={'https://github.com/geilson25.png'}
+              rating={3}
+              description={'Recife'}
+            />
+            <UserInformation
+              name={'Geilson'}
+              picture={'https://github.com/geilson25.png'}
+              rating={3}
+              description={'Recife'}
+            />
+          </ProfissionaisContainer>
+        </ProfissionaisPaper>
+
+    </Container>
+    
     </div>
+
   );
+  
 }
